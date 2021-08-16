3f534b7f14d2d1d681b5dc1a8916e52873856d31
Potosyntesis <71467898+Potosyntesis@users.noreply.github.com>
2021-08-16 19:51:42 +1200

    update

--- Assets/blendTreeAni.cs
+++ Assets/blendTreeAni.cs
@@ -14,7 +14,45 @@ public class blendTreeAni : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && VelZ <= 1f)
+        {
+            VelZ += 0.5f * Time.deltaTime;
+        }else if (Input.GetKey(KeyCode.W) && VelZ <= 0.5f)
+        {
+            VelZ += 0.5f * Time.deltaTime;
+        }
+        else if(VelZ >= 0)
+        {
+            VelZ -= 0.5f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && VelZ <= 1f)
+        {
+            VelZ += 0.5f * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.W) && VelZ <= 0.5f)
+        {
+            VelZ += 0.5f * Time.deltaTime;
+        }
+        else if (VelZ >= 0)
+        {
+            VelZ -= 0.5f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && VelZ <= 1f)
+        {
+            VelZ += 0.5f * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.W) && VelZ <= 0.5f)
+        {
+            VelZ += 0.5f * Time.deltaTime;
+        }
+        else if (VelZ >= 0)
+        {
+            VelZ -= 0.5f * Time.deltaTime;
+        }
+
         ani.SetFloat("VelocityX", VelX);
         ani.SetFloat("VelocityZ", VelZ);
     }
